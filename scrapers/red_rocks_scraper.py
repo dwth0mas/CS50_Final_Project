f8bf74f676133410c24a78934c89c770fb880923
dythomas-xactly <dythomas@xactlycorp.com>
2020-12-20 19:02:10 -0700

    adding the 3 other scraper files

--- scrapers/red_rocks_scraper.py
+++ scrapers/red_rocks_scraper.py
@@ -1,10 +1,9 @@
-import bs4
-from urllib.request import urlopen as u_req
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as Soup
 
 red_rocks_url = "https://www.redrocksonline.com/events/"
 
-u_client = u_req(red_rocks_url)
+u_client = urlopen(red_rocks_url)
 red_rocks_events_page_html = u_client.read()
 u_client.close()
 
@@ -12,13 +11,13 @@ red_rocks_events_soup = Soup(red_rocks_events_page_html, "html.parser")
 
 event_containers = red_rocks_events_soup.findAll("div", {"class": "card card-event event-month-active "
                                                                   "event-filter-active"})
-print(event_containers[0])
-container = event_containers[0]
-
-# event_container_dates = event_containers.findAll("div", {"class": "date"})
 
 for container in event_containers:
-    print(container.find("div", {"class": "date"}).text.strip())
-    print(container.find("h3", {"class": "card-title"}).text.strip())
-    print(container.find("div", {"class": "buttons"}).text.strip())
+    event_title = container.find("h3", {"class": "card-title"}).text.strip()
+    event_date = container.find("div", {"class": "date"}).text.strip()
+    ticket_link = container.find("a", {"class": "btn btn-white small"})['href']
+
+    print(event_title)
+    print(event_date)
+    print(ticket_link)
     print("\n")
